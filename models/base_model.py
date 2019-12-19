f89dca79e8b558d2d503f0fb1384faf18a6958d7
Gareth Brickman <977@holbertonschool.com>
2019-12-19 22:20:04 +0000

    [BUGFIX] Typo for _sa_instance in to_dict()

--- models/base_model.py
+++ models/base_model.py
@@ -69,8 +69,8 @@ class BaseModel:
         my_dict["__class__"] = str(type(self).__name__)
         my_dict["created_at"] = self.created_at.isoformat()
         my_dict["updated_at"] = self.updated_at.isoformat()
-        if "sa_instance_state" in my_dict.keys():
-            del my_dict["sa_instance_state"]
+        if "_sa_instance_state" in my_dict.keys():
+            del my_dict["_sa_instance_state"]
         return my_dict
 
     def delete(self):
