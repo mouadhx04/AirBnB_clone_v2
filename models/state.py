a9db5332d78ba178d77d5259d411d60ae91c0e9f
Gareth Brickman <977@holbertonschool.com>
2019-12-19 01:05:11 +0000

    [TASK] Add #6 requirements

--- models/state.py
+++ models/state.py
@@ -1,11 +1,35 @@
 #!/usr/bin/python3
 """This is the state class"""
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+# TO DO:
+# cities getter
+#for FileStorage: getter attribute cities that returns
+#the list of City instances with state_id equals to the
+#current State.id =>
+#It will be the FileStorage relationship between State and City
+
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
+    __tablename__: SQL table
+    cities: for DBStorage, creates relationship for class City to state table
     """
-    name = ""
+    name = Column(String(128), nullable=False)
+    __tablename__ = 'states'
+    cities = relationship("City", backref="state")
+
+    @property
+    def cities(self):
+        """ Getter
+        """
+        pass
+        # objects = storage.all(cities)
+        # key = my_list[0] + '.' + my_list[1]
+        # if key in objects:
+        #     print(objects[key])
