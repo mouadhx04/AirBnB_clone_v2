3e015adf5055cb7b0872b7b919b3c4cf6f1488b9
Gareth Brickman <977@holbertonschool.com>
2019-12-20 19:24:17 +0000

    [BUGFIX] Various minor bug fixes

--- models/user.py
+++ models/user.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This is the user class"""
-from models.base_model import BaseModel, Base
+from models.base_model import BaseModel
+from models.base_model import Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
