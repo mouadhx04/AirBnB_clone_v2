8fc6df38be24d012ef54e1bb8049522f84680021
Jose-AlvarezdeLugo <alvarezdelugo.jose@gmail.com>
2020-01-03 21:44:04 -0500

    [BUGFIX] Change syntax

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,10 +2,13 @@
 # Bash script to set up web servers for the deployment of web_static
 sudo apt-get -y update
 sudo apt-get -y install nginx
-mkdir -p /data/web_static/shared/
-mkdir -p /data/web_static/releases/test/
-echo -e '<html>\n<head>\n</head>\n<body>\nHolberton fake file\n</body>\n</html>' > /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo service nginx start
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+echo 'temp file' | /data/web_static/releases/test/index.html
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i "42i location /hbnb_static {\nalias /data/web_static/current;\n}" /etc/nginx/sites-available/default
+sudo service nginx reload
 sudo service nginx restart
