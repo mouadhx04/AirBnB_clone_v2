4cb4b005a9e8470000963958792f5b3a0c8c091d
jgadelugo <alvarezdelugo.jose@gmail.com>
2020-01-21 21:34:28 +0000

    [PROJECT] Flask set up initial commit

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    """ print Hello HBNB"""
+    return 'Hello HBNB!'
+
+
+if __name__ == '__main__':
+    app.run()
+    app.url_map.strict_slashes = False
