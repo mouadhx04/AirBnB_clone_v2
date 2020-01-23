15bc53a80d6ff4e87742b869dfb95af09ab6c514
Jose Alvarez de Lugo <alvarezdelugo.jose@gmail.com>
2020-01-22 21:42:34 -0500

    [TASK] return template with n in h1 if int

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+from flask import Flask, escape, render_template
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_world():
+    """ print Hello HBNB"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hello_hbnb():
+    """ print HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def grab_text(text):
+    """ print HBNB"""
+    return "C %s" % escape(text.replace("_", " "))
+
+
+@app.route('/python', defaults={'text': 'is cool'})
+@app.route('/python/<text>')
+def grab_python_text(text):
+    """ print HBNB"""
+    return "Python %s" % escape(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>')
+def display_int(n):
+    """ print n is a number if int"""
+    return '%d is a number' % n
+
+
+@app.route('/number_template/<int:n>')
+def render_num(n):
+    """ return HTML page with n in h1 with n is int """
+    return render_template('5-number.html', n=n)
+
+if __name__ == '__main__':
+    app.run()
+    app.url_map.strict_slashes = False
