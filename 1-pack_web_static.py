f177f6af35e1cc3911a9dd95c8bd38cb18a22dd2
root <root@Mouadh.localdomain>
2022-01-14 13:21:40 +0100

    Compress before sending

---
+++ 1-pack_web_static.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Fabric script that generates a tgz archive from the contents of the web_static
+folder of the AirBnB Clone repo
+"""
+
+from datetime import datetime
+from fabric.api import local
+from os.path import isdir, exists
+
+
+def do_pack():
+    """
+    generate tgz archive using fabric
+    """
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    file_path = "versions/web_static_{}.tgz".format(date)
+    if isdir("versions") is False:
+        local(" mkdir versions")
+    local('tar -cvzf ' + file_path + ' web_static')
+    if exists(file_path):
+        return file_path
+    return None
