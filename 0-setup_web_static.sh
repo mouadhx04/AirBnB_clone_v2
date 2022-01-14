fdbc2f44d5799f41c7ef7edbcb585e440ca19b81
root <root@Mouadh.localdomain>
2022-01-14 13:03:06 +0100

    Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,7 +6,7 @@ sudo apt-get install -y nginx
 mkdir -p  /data/web_static/
 mkdir -p /data/web_static/releases/test/
 mkdir /data/web_static/shared/
-echo "Hello AirBnb" | sudo tee /data/web_static/releases/test/index.html
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test /data/web_static/current 
 chown ubuntu:ubuntu -hR /data/
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
