156e462daced4fb719928df628aa86f8979d2bce
root <root@Mouadh.localdomain>
2022-01-30 18:20:43 +0100

    C is fun!

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application module
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hbnb():
+    """
+        fn to display Hello HBNB!
+        in the route page
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_():
+    """
+        fn to display HBNB
+        in the route page
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def hbnb_c(text):
+    """
+        fn to display a text
+        in the route page
+    """
+    txt = text.replace('_', ' ')
+    return 'C {}'.format(txt)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
