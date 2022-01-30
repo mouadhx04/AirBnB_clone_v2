1b5f5d13637792900de4472de807e9dc904bb00b
root <root@Mouadh.localdomain>
2022-01-30 20:31:11 +0100

    List of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,26 +1,31 @@
 #!/usr/bin/python3
 """
-flask model
+starts a Flask web application module
 """
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+
+
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardown_data(self):
+def hbnb_teardown_app():
     """
-        refrech data
+        close the current SQLAlchemy Session
     """
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """ return all states in the db  """
-    states = storage.all(State)
-    return render_template('7-states_list.html', states=states)
+@app.route('/state_list', strict_slashes=False)
+def hbnb_state_list():
+    """
+        fn for fetching data
+        from the storage engine
+    """
+    states_list = storage.all(State).values()
+    return render_template('7-states_list.html', states=states_list)
 
 
 if __name__ == "__main__":
